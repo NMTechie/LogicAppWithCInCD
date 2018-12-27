6fce6f2b21f96d5c93348f0bde4d5714fd049ad5
NMTechie <nileshranjan.mitra@yahoo.in>
2018-12-27 12:55:26 +0000

    Update CreateBkEndForC32FromiPaaS.ps1

--- CustomScripts/CreateBkEndForC32FromiPaaS.ps1
+++ CustomScripts/CreateBkEndForC32FromiPaaS.ps1
@@ -1,2 +1,2 @@
 $apicontext= New-AzureRmApiManagementContext -ResourceGroupName "ipaas-int-we-apim-01" -ServiceName "nok-ipaas-int-we-apm-01"
-New-AzureRmApiManagementBackend -Context $apicontext -BackendId "C32BackEnd" -Protocol http -url "https://10.130.221.176/" -SkipCertificateChainValidation 1
+New-AzureRmApiManagementBackend -Context $apicontext -BackendId "C32BackEnd" -Protocol http -url "https://<<the ip of the back-end api domain that use self-sign certificate for SSL>>/" -SkipCertificateChainValidation 1
